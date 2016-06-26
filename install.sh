e31a4dfcacd0e9610a62c87a6acbf59b4117ba91
Rizki Mufrizal <mufrizalrizki@gmail.com>
2016-06-26 21:33:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,9 +34,6 @@ sudo add-apt-repository -y ppa:otto-kesselgulasch/gimp
 echo "tambah repo libree office"
 sudo add-apt-repository -y ppa:libreoffice/ppa
 
-echo "tambah repo docky"
-sudo add-apt-repository -y ppa:ricotz/docky
-
 echo "tambah repo vlc"
 sudo add-apt-repository -y ppa:videolan/stable-daily
 
@@ -80,7 +77,7 @@ echo mariadb-server-10.1 mysql-server/root_password password root | sudo debconf
 echo mariadb-server-10.1 mysql-server/root_password_again password root | sudo debconf-set-selections
 
 echo "install aplikasi"
-sudo apt install -y mariadb-server mariadb-client atom chromium-browser git gimp vlc browser-plugin-vlc openjdk-8-jdk openjdk-8-jre icedtea-8-plugin icedtea-plugin postgresql-9.5 postgresql-contrib libpq-dev pgadmin3 gimp-data gimp-plugin-registry gimp-data-extras apache2 php7.0-cli php7.0-mysql php7.0-fpm php7.0-gd php7.0 libapache2-mod-php7.0 php7.0-mcrypt php7.0-common php7.0-snmp snmp php7.0-curl php7.0-json php7.0-pgsql php7.0-mbstring php7.0-zip php7.0-xmlrpc php7.0-cgi php-pear php7.0-dev php7.0-imap mysql-server mysql-client clementine xclip sublime-text-installer vim zip unzip lib32ncurses5 lib32gomp1 lib32z1-dev
+sudo apt install -y mariadb-server mariadb-client atom chromium-browser git gimp vlc browser-plugin-vlc openjdk-8-jdk openjdk-8-jre icedtea-8-plugin icedtea-plugin postgresql-9.5 postgresql-contrib libpq-dev pgadmin3 gimp-data gimp-plugin-registry gimp-data-extras apache2 php7.0-cli php7.0-mysql php7.0-fpm php7.0-gd php7.0 libapache2-mod-php7.0 php7.0-mcrypt php7.0-common php7.0-snmp snmp php7.0-curl php7.0-json php7.0-pgsql php7.0-mbstring php7.0-zip php7.0-xmlrpc php7.0-cgi php-pear php7.0-dev php7.0-imap clementine xclip sublime-text-installer vim zip unzip lib32ncurses5 lib32gomp1 lib32z1-dev
 
 echo "konfigurasi php"
 sudo chmod 777 -R /var/www/*
