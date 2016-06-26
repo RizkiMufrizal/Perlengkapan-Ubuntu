c34b67f6e75fee90a63e00f8a9bed5985894d169
Rizki Mufrizal <mufrizalrizki@gmail.com>
2016-06-26 21:43:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,19 @@
 # Distributed under terms of the MIT license.
 #
 
-sudo add-apt-repository ppa:no1wantdthisname/ppa
+sudo add-apt-repository -y ppa:saiarcot895/myppa
 sudo apt update
-sudo apt install libfreetype6 fontconfig-infinality
+sudo apt install -y apt-fast
+
+sudo add-apt-repository -y ppa:no1wantdthisname/ppa
+sudo apt-fast update
+sudo apt-fast install -y libfreetype6 fontconfig-infinality
 sudo rm /etc/fonts/conf.avail/52-infinality.conf
 sudo ln -s /etc/fonts/infinality/infinality.conf /etc/fonts/conf.avail/52-infinality.conf
 
 echo "tambah repo java 8"
-sudo add-apt-repository ppa:no1wantdthisname/openjdk-fontfix
-sudo add-apt-repository ppa:openjdk-r/ppa
+sudo add-apt-repository -y ppa:no1wantdthisname/openjdk-fontfix
+sudo add-apt-repository -y ppa:openjdk-r/ppa
 
 echo "tambah repo git"
 sudo add-apt-repository -y ppa:git-core/ppa
@@ -53,7 +57,7 @@ echo "tambah repo gcc"
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
 echo "tambah repo mariadb"
-sudo apt-get install software-properties-common
+sudo apt-fast install -y software-properties-common
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 sudo add-apt-repository 'deb [arch=amd64,i386] http://kartolo.sby.datautama.net.id/mariadb/repo/10.1/ubuntu xenial main'
 
@@ -63,13 +67,13 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
 sudo apt-key add -
 
 echo "update"
-sudo apt update
+sudo apt-fast update
 
 echo "upgrade"
-sudo apt upgrade -y
+sudo apt-fast upgrade -y
 
 echo "dist-upgrade"
-sudo apt dist-upgrade -y
+sudo apt-fast dist-upgrade -y
 
 echo "Konfigurasi Mariadb"
 sudo apt-get install -y debconf-utils
@@ -77,7 +81,7 @@ echo mariadb-server-10.1 mysql-server/root_password password root | sudo debconf
 echo mariadb-server-10.1 mysql-server/root_password_again password root | sudo debconf-set-selections
 
 echo "install aplikasi"
-sudo apt install -y mariadb-server mariadb-client atom chromium-browser git gimp vlc browser-plugin-vlc openjdk-8-jdk openjdk-8-jre icedtea-8-plugin icedtea-plugin postgresql-9.5 postgresql-contrib libpq-dev pgadmin3 gimp-data gimp-plugin-registry gimp-data-extras apache2 php7.0-cli php7.0-mysql php7.0-fpm php7.0-gd php7.0 libapache2-mod-php7.0 php7.0-mcrypt php7.0-common php7.0-snmp snmp php7.0-curl php7.0-json php7.0-pgsql php7.0-mbstring php7.0-zip php7.0-xmlrpc php7.0-cgi php-pear php7.0-dev php7.0-imap clementine xclip sublime-text-installer vim zip unzip lib32ncurses5 lib32gomp1 lib32z1-dev
+sudo apt-fast install -y zsh mariadb-server mariadb-client atom chromium-browser git gimp vlc browser-plugin-vlc openjdk-8-jdk openjdk-8-jre icedtea-8-plugin icedtea-plugin postgresql-9.5 postgresql-contrib libpq-dev pgadmin3 gimp-data gimp-plugin-registry gimp-data-extras apache2 php7.0-cli php7.0-mysql php7.0-fpm php7.0-gd php7.0 libapache2-mod-php7.0 php7.0-mcrypt php7.0-common php7.0-snmp snmp php7.0-curl php7.0-json php7.0-pgsql php7.0-mbstring php7.0-zip php7.0-xmlrpc php7.0-cgi php-pear php7.0-dev php7.0-imap clementine xclip sublime-text-installer vim zip unzip lib32ncurses5 lib32gomp1 lib32z1-dev
 
 echo "konfigurasi php"
 sudo chmod 777 -R /var/www/*
